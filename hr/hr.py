553467e93359f62bbf0f271fb954cc77b2ac1b48
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2016-12-13 14:00:51 +0100

    Add function added in HR.

--- hr/hr.py
+++ hr/hr.py
@@ -1,6 +1,7 @@
 # data structure:
 # id: string
-#     Unique and random generated (at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter)
+#     Unique and random generated (at least 2 special char()expect: ';'),
+#     2 number, 2 lower and 2 upper case letter)
 # name: string
 # birth_date: number (year)
 
@@ -27,7 +28,15 @@ def start_module():
 
     # your code
 
-    pass
+    sales_options = ["Show Table","Add","Remove","Update","Get oldest person","Get persons closest to average"]
+    keep_menu = True
+    while keep_menu:
+        ui.print_menu('Sales Main Menu',sales_options,'Back To Menu')
+        try:
+            keep_menu = choose_hr()
+        except KeyError as err:
+            ui.print_error_message(err)
+
 
 
 def show_table(table):
@@ -58,6 +67,17 @@ def add(table):
     """
 
     # your code
+
+    inputs = ui.get_inputs(["\n1. ID (Unique and random generated (at least 2 special char()\
+ expect: ';'), 2 number, 2 lower and 2 upper case letter))", "Name", "Birth date"], "\nPlease add a worker.")
+
+    one_input = inputs[0], inputs[1], inputs[2]
+    one_input_list = list(one_input) # we must do it
+    table = data_manager.get_table_from_file('hr/persons.csv')
+    table.append(one_input_list)
+    data_manager.write_table_to_file('hr/persons.csv', table)
+    print("\n Added. \n \n")
+    #print(table)
 
     return table
 
@@ -115,3 +135,21 @@ def get_persons_closest_to_average(table):
     # your code
 
     pass
+
+def choose_hr():
+    inputs = ui.get_inputs(["Please enter a number: "], "")
+    option = inputs[0]
+    data = data_manager.get_table_from_file('hr/persons.csv')
+    if option == "1":
+        show_table(data)
+        return True
+    elif option == "2":
+        add(data_manager.get_table_from_file('hr/persons.csv'))
+        return True
+    elif option == "4":
+        update(data,ui.get_inputs(["Please enter ID"],"Pick the ID to update"))
+        return True
+    elif option == "0":
+        return False
+    else:
+        raise KeyError("There is no such option.")

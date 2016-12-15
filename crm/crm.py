50ad8f72870f1f80ab7daac003358fb85bc3d523
ika.grabon <ika.grabon@gmail.com>
2016-12-15 20:56:13 +0100

    fixed some functions

--- crm/crm.py
+++ crm/crm.py
@@ -226,26 +226,34 @@ def show_nicelist(nice_list):
 
 def generate_random():
     import random
-    import string
     # Unique and randomly generated (at least 2 special char()expect: ';'), 2 number, 2 lower and 2 upper case letter)
     special_chars = ['!', '@', '#', '$', '%', '&']
     digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    letters = string.ascii_lowercase
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+               'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z']
     table = [[special_chars], [digits], [letters]]
 
     generated = ''
     is_unique = False
-    new_id = []
-    for row in table:
-        new_id.append(row[0])
+    list_of_id = []
 
     while not is_unique:
+        list_of_char = []
+        new_id = ''
         is_unique = True
         for i in range(2):
-            generated += str(special_chars[random.randint(0, len(special_chars)-1)])
-            generated += str(digits[random.randint(0, len(digits)-1)])
-            generated += str(letters[random.randint(0, len(letters)-1)])
-            generated += str(letters[random.randint(0, len(letters)-1)].upper())
-        if generated in new_id:
-            is_unique = False
+            list_of_char.append(str(special_chars[random.randint(0, len(special_chars)-1)]))
+            list_of_char.append(str(digits[random.randint(0, len(digits)-1)]))
+            list_of_char.append(str(letters[random.randint(0, len(letters)-1)]))
+            list_of_char.append(str(letters[random.randint(0, len(letters)-1)].upper()))
+        for i in range(len(list_of_char)):
+            char = random.choice(list_of_char)
+            new_id += char
+            list_of_char.remove(char)
+        if new_id not in list_of_id:
+            generated += new_id
+            is_unique = True
+    for row in table:
+        list_of_id.append(row[0])
     return generated
